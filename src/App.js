21a270e44aae50ed7f954f0fc2b7d66b779f2285
xleonhunter <xleonhunter@gmail.com>
2021-12-21 19:20:48 -0500

    optimized imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import './scss/AppStyles.scss';
-import {Navbar, NavDropdown, Nav, Container, Row} from 'react-bootstrap';
 import 'regenerator-runtime/runtime'
-import { login, logout } from './utils'
 import './global.css'
 import MetaData from './components/MetaData'
 import NavbarComponent from './components/NavbarComponent'
-
-import getConfig from './config'
-const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
 export default function App() {
   return (
